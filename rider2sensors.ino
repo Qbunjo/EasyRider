3cfaddfe5ee81013cc29a756c8580c40971969be
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2020-02-26 10:32:54 +0100

    Create rider2sensors.ino

---
+++ rider2sensors.ino
@@ -0,0 +1,81 @@
+#include <NewPing.h>
+#define TRIGGER_PINL  2  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define TRIGGER_PINR  6
+#define ECHO_PINL     3  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define ECHO_PINR     7
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+int distanceL = 0;
+int distanceR = 0;
+
+NewPing sonar1(TRIGGER_PINL, ECHO_PINL, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+NewPing sonar2(TRIGGER_PINR, ECHO_PINR, MAX_DISTANCE);
+
+void setup() {
+  // these pins have to be PWM capable
+  //engineL
+  pinMode (5, OUTPUT);
+  pinMode (6, OUTPUT);
+  //engineR
+  pinMode (10, OUTPUT);
+  pinMode (11, OUTPUT);
+
+  Serial.begin(9600);
+  Serial.println("All systems ready");
+}
+
+void loop() {
+
+  delay(50);
+  distanceL = sonar1.ping_cm();
+  delay(50);
+  distanceR = sonar2.ping_cm();
+
+  if (distanceL == 0) {
+    distanceL = 100;
+  }
+  if (distanceR == 0) {
+    distanceR = 100;
+  }
+
+  if (distanceL < 15 and distanceR < 15) {
+    Reverse();
+  }
+  if (distanceL < 30) {
+    ObstacleL();
+  }
+  if (distanceR < 30) {
+    ObstacleR();
+  }
+
+  { GoAhead();
+    Serial.println(distanceL + " " + distanceR);
+  }
+}
+void GoAhead() {
+  analogWrite(5, 55);
+  analogWrite(6, 0);
+  analogWrite(10, 55);
+  analogWrite(11, 0);
+  Serial.println("Foreward");
+}
+void Reverse() {
+  analogWrite(5, 0);
+  analogWrite(6, 45);
+  analogWrite(10, 0);
+  analogWrite(11, 45);
+  Serial.println("Reverse");
+}
+void ObstacleL() {
+  analogWrite(5, 55);
+  analogWrite(6, 0);
+  analogWrite(10, 0);
+  analogWrite(11, 0);
+  Serial.println("Avoid obstacle L");
+}
+void ObstacleR() {
+  analogWrite(5, 0);
+  analogWrite(6, 55);
+  analogWrite(10, 0);
+  analogWrite(11, 0);
+  Serial.println("Avoid obstacle R");
+}
